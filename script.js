d0fc89433e4c395207eed53d684af9e4c1ce645f
siadsom-spec <siadsom@gmail.com>
2026-08-03 12:12:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function showPage(pageId) {
+  const pages = document.querySelectorAll('.page');
+  pages.forEach(page => {
+    page.classList.remove('active');
+  });
 
+  const selectedPage = document.getElementById(pageId);
+  if (selectedPage) {
+    selectedPage.classList.add('active');
+  }
+}
